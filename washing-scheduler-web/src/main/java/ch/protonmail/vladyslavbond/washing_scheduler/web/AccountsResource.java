a3054ae45d55b9167dcc6ae4e5d9ceaf6bbff0ba
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2015-12-03 11:32:47 +0100

    This resolves Wbondar/washing-scheduler#11 and resolves Wbondar/washing-scheduler#5 and closes Wbondar/washing-scheduler#1.

--- washing-scheduler-web/src/main/java/ch/protonmail/vladyslavbond/washing_scheduler/web/AccountsResource.java
+++ washing-scheduler-web/src/main/java/ch/protonmail/vladyslavbond/washing_scheduler/web/AccountsResource.java
@@ -2,6 +2,8 @@ package ch.protonmail.vladyslavbond.washing_scheduler.web;
 
 import java.net.URI;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,9 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 
 import org.scribe.model.OAuthRequest;
 import org.scribe.model.Token;
@@ -24,8 +24,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Path("accounts")
 public class AccountsResource {
+	@Context
+	private HttpServletResponse response;
 	@Context 
-	private HttpSession session;
+	private HttpServletRequest request;
+	
+	private HttpSession getSession() {
+		return request.getSession();
+	}
 	
 	private final static ObjectMapper getObjectMapper() {
 		return new ObjectMapper();
@@ -39,25 +45,31 @@ public class AccountsResource {
 	}
 	
 	@GET
-	@Path("create/{service}/callback")
+	@Path("callback/{service}")
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response callback(@PathParam("service") WashingSchedulerOAuthService service, @QueryParam("code") String code) throws Exception {
-		Token accessToken = service.getAccessToken(Token.empty(), new Verifier(code));
-		OAuthRequest request = new OAuthRequest(Verb.GET, service.getProperty("profile")); /* https://graph.facebook.com/me */
-		service.signRequest(accessToken, request);
-		org.scribe.model.Response oauthResponse = request.send();
-		if (oauthResponse.isSuccessful()) {
-			final String id = getObjectMapper().readTree(oauthResponse.getBody()).get("id").asText();
-			service.setProperty(session, "id", id);
-			return Response.ok().entity("Well, hello, user of ID " + id + "!").build();
+		try
+		{
+			Token accessToken = service.getAccessToken(Token.empty(), new Verifier(code));
+			OAuthRequest request = new OAuthRequest(Verb.GET, service.getProperty("profile"));
+			service.signRequest(accessToken, request);
+			org.scribe.model.Response oauthResponse = request.send();
+			if (oauthResponse.isSuccessful()) {
+				final String id = getObjectMapper().readTree(oauthResponse.getBody()).get("id").asText();
+				service.setProperty(getSession(), "id", id);
+				return Response.ok().entity("Well, hello, user of ID " + id + "!").build();
+			}
+			return Response.seeOther(new URI("create")).build();
+		} catch (Exception e) {
+			e.printStackTrace(response.getWriter());
+			throw e;
 		}
-		return Response.seeOther(new URI("create")).build();
 	}
 	
 	@GET
 	@Path("create")
 	@Produces(MediaType.TEXT_HTML)
 	public Response create() {
-		return Response.ok().entity("<!DOCTYPE html><html><head></head><body><form method='POST' action='accounts/create/" + FACEBOOK.name() +"'><label>Sign in with Facebook.<input type='submit'/></label></form></body></html>").build();
+		return Response.ok().entity("<!DOCTYPE html><html><head></head><body><form method='POST' action='https://washing-scheduler.herokuapp.com/accounts/create/" + FACEBOOK.name() +"'><label>Sign in with Facebook.<input type='submit'/></label></form></body></html>").build();
 	}
 }
