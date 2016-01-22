624221770b16fc86eb72c71f3d6188f3c8b9f38e
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2016-01-22 12:37:54 +0100

    This resolves Wbondar/washing-scheduler#41 and resolves Wbondar/washing-scheduler#53.

--- washing-scheduler-web/src/main/java/ch/protonmail/vladyslavbond/washing_scheduler/web/RequestsResource.java
+++ washing-scheduler-web/src/main/java/ch/protonmail/vladyslavbond/washing_scheduler/web/RequestsResource.java
@@ -10,7 +10,18 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.chrono.Chronology;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -24,6 +35,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import static ch.protonmail.vladyslavbond.washing_scheduler.datasource.NativeConnectionFactory.*;
 import static ch.protonmail.vladyslavbond.washing_scheduler.web.ViewFactory.*;
@@ -71,7 +83,10 @@ public class RequestsResource extends WashingSchedulerResource {
 				try (ResultSet resultSet = queueLookUpStatement.executeQuery();) 
 				{
 					final ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+					
 					final ObjectMapper objectMapper = getObjectMapper();
+					final DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.SHORT).withLocale(getHttpServletRequest().getLocale());
+					
 					while (resultSet.next()) 
 					{
 
@@ -79,7 +94,18 @@ public class RequestsResource extends WashingSchedulerResource {
 						
 						int i = resultSetMetaData.getColumnCount();
 						while (i > 0) {
-							tuple.put(resultSetMetaData.getColumnLabel(i), resultSet.getObject(i));
+							Object value = resultSet.getObject(i);
+							System.out.println(value.getClass().getName());
+							if (value instanceof java.sql.Timestamp) {
+								java.sql.Timestamp timestamp = ((java.sql.Timestamp)value);
+								LocalDateTime localDateTime = timestamp.toLocalDateTime();
+								value = localDateTime.format(formatter);
+							} else if (value instanceof org.postgresql.util.PGInterval) {
+								org.postgresql.util.PGInterval interval = ((org.postgresql.util.PGInterval)value);
+								int amountOfMinutes = interval.getHours() * 60 + interval.getMinutes();
+								value = String.format("%02d:%02d", amountOfMinutes / 60, amountOfMinutes % 60);
+							}
+							tuple.put(resultSetMetaData.getColumnLabel(i), value);	
 							i--;
 						}
 				
@@ -142,22 +168,59 @@ public class RequestsResource extends WashingSchedulerResource {
 	@POST
 	@Path("create")
 	@Produces(MediaType.TEXT_HTML)
-	public Response create(@FormParam("time") ReservationDuration reservationTime) throws URISyntaxException, IOException {
+	public Response create(@FormParam("datepicker") String inputDate, @FormParam("timepicker") String inputTime, @FormParam("duration") ReservationDuration reservationDuration) throws URISyntaxException, IOException {
+		/*
+		 * Check if the user is authenticated to the system.
+		 * If not, redirect them.
+		 */
 		if (getHttpSession() == null || getHttpSession().getAttribute("id") == null) {
 			getHttpSession().setAttribute("myCallback", REQUESTS_CREATE);
 			return Response
 					.seeOther(new URI(WashingSchedulerOAuthService.FACEBOOK.getAuthorizationUrl(Token.empty())))
 					.build();
 		}
+		
+		/*
+		 * Convert input date and time to timestamp object,
+		 * to use it to query the database.
+		 * If input is invalid, set the status to "bad request".
+		 */
+		java.sql.Timestamp effectiveAt = null;
+		try
+		{
+			if (inputDate == null || inputDate.isEmpty()) {
+				throw new RuntimeException("Date is missing.");
+			}
+			if (inputTime == null || inputTime.isEmpty()) {
+				throw new RuntimeException("Time is missing.");
+			}
+			effectiveAt = java.sql.Timestamp.valueOf(inputDate.concat(inputTime));
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			throw new RuntimeException(String.format("Unacceptable input. Got: \"%s\" and \"%s\" for date and time, respectively.", inputDate, inputTime));
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+		
+		/*
+		 * If everything went fine this far,
+		 * query the database for reservation.
+		 * If something went wrong, set the status to "server error".
+		 */
 		try (Connection connection = getConnection();) {
-			try (CallableStatement statement = connection.prepareCall("{CALL request_create(?::SMALLINT, NOW(), ?::INTERVAL)}");) {
+			try (CallableStatement statement = connection.prepareCall("{CALL request_create(?::SMALLINT, ?::TIMESTAMP, ?::INTERVAL)}");) {
 				statement.setObject(1, getHttpSession().getAttribute("id"));
-				statement.setObject(2, reservationTime);
+				if (effectiveAt == null || effectiveAt.toLocalDateTime().isBefore(LocalDateTime.now())) {
+					effectiveAt = java.sql.Timestamp.valueOf(LocalDateTime.now());
+				}
+				statement.setTimestamp(2, effectiveAt);
+				statement.setObject(3, reservationDuration);
 				if (statement.execute()) {
 					return Response.seeOther(QUEUE_URI).build();
 				}
 			}
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		/* TODO Implement comprehensive error messages. */
