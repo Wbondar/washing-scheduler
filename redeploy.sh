bf5578c5c4e1beb1b4aba9c888fe9ea52c24807b
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2016-01-26 17:05:57 +0100

    This resolves Wbondar/washing-scheduler#27.

--- redeploy.sh
+++ redeploy.sh
@@ -6,6 +6,25 @@ native2ascii washing-scheduler-web/src/main/resources/footer.properties washing-
 native2ascii washing-scheduler-web/src/main/resources/index.properties washing-scheduler-web/src/main/resources/index.properties 
 native2ascii washing-scheduler-web/src/main/resources/create.properties washing-scheduler-web/src/main/resources/create.properties 
 native2ascii washing-scheduler-web/src/main/resources/queue.properties washing-scheduler-web/src/main/resources/queue.properties 
+
+native2ascii washing-scheduler-web/src/main/resources/header_pl.properties washing-scheduler-web/src/main/resources/header_pl.properties 
+native2ascii washing-scheduler-web/src/main/resources/menu_pl.properties washing-scheduler-web/src/main/resources/menu_pl.properties 
+native2ascii washing-scheduler-web/src/main/resources/aside_pl.properties washing-scheduler-web/src/main/resources/aside_pl.properties 
+native2ascii washing-scheduler-web/src/main/resources/footer_pl.properties washing-scheduler-web/src/main/resources/footer_pl.properties 
+native2ascii washing-scheduler-web/src/main/resources/index_pl.properties washing-scheduler-web/src/main/resources/index_pl.properties 
+native2ascii washing-scheduler-web/src/main/resources/create_pl.properties washing-scheduler-web/src/main/resources/create_pl.properties 
+native2ascii washing-scheduler-web/src/main/resources/queue_pl.properties washing-scheduler-web/src/main/resources/queue_pl.properties 
+native2ascii washing-scheduler-web/src/main/resources/sessions_create_pl.properties washing-scheduler-web/src/main/resources/sessions_create_pl.properties 
+
+native2ascii washing-scheduler-web/src/main/resources/header_ru.properties washing-scheduler-web/src/main/resources/header_ru.properties 
+native2ascii washing-scheduler-web/src/main/resources/menu_ru.properties washing-scheduler-web/src/main/resources/menu_ru.properties 
+native2ascii washing-scheduler-web/src/main/resources/aside_ru.properties washing-scheduler-web/src/main/resources/aside_ru.properties 
+native2ascii washing-scheduler-web/src/main/resources/footer_ru.properties washing-scheduler-web/src/main/resources/footer_ru.properties 
+native2ascii washing-scheduler-web/src/main/resources/index_ru.properties washing-scheduler-web/src/main/resources/index_ru.properties 
+native2ascii washing-scheduler-web/src/main/resources/create_ru.properties washing-scheduler-web/src/main/resources/create_ru.properties 
+native2ascii washing-scheduler-web/src/main/resources/queue_ru.properties washing-scheduler-web/src/main/resources/queue_ru.properties 
+native2ascii washing-scheduler-web/src/main/resources/sessions_create_ru.properties washing-scheduler-web/src/main/resources/sessions_create_ru.properties 
+
 mvn clean compile package
 cd washing-scheduler-web
 mvn jetty:run
